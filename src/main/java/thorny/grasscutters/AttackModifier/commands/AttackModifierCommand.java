a7ce4d55d0202c2bfeba0ceec675b37f3447e911
Snoobii Yeo <215ralph_yeo_san_ren@njcgclassroom.com>
2022-10-11 19:15:07 +0800

    omg fix?

--- src/main/java/thorny/grasscutters/AttackModifier/commands/AttackModifierCommand.java
+++ src/main/java/thorny/grasscutters/AttackModifier/commands/AttackModifierCommand.java
@@ -21,18 +21,20 @@ import emu.grasscutter.data.GameData;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.UIDefaults.ActiveValue;
-
-import com.mchange.v2.c3p0.management.ActiveManagementCoordinator;
 
 // Command usage
-@Command(label = "attack", aliases = {"at","am"}, usage = "[IdOfObject] [radiusSpawnFromSelf]", targetRequirement = TargetRequirement.NONE)
+@Command(label = "attack", aliases = {"at","am","snoospawn"}, usage = "[IdOfObject] [radiusSpawnFromSelf] [height] [count] [rotXupwards] [rotYHorizontalFromSELF] [rotZlastrot]\n\nID of object is the id of the object in the handbook\nradiusSpawnFromSelf is the distance between u and the object spawned when atking\nheight is the distance above u the gadget is spawned\nrotX change the vertical rotation of object\nrotY changed the horizontal rotation from where u are facing [180 will shoot behind u always]\nrotZ is the 3rd cross vector rotation to rot y and x", targetRequirement = TargetRequirement.NONE)
 public class AttackModifierCommand implements CommandHandler {
 
     static int gadgetId = 0;
     static int itemId = 0;
     static int monsterId = 0;
     static float radius = 0;
+    static float rotX = 0;
+    static float rotY = 0;
+    static float rotZ = 0;
+    static float height = 0;
+    static int count = 1;
     static List<EntityGadget> activeGadgets = new ArrayList<>(); // Current gadgets
     static List<EntityGadget> removeGadgets = new ArrayList<>(); // To be removed gadgets
     static List<EntityVehicle> activeVehicles = new ArrayList<>(); // Current gadgets
@@ -45,14 +47,39 @@ public class AttackModifierCommand implements CommandHandler {
          * Command usage available to check the gadgets before adding them
          * Just spawns the gadget where the player is standing, given the id
          */
-        float radiusFromArgs = 0;
+        float radiusFromArgs = 0f;
         int thing = 0;
+        float heightFromArgs = 0f;
+        float rotXFromArgs = 0f;
+        float rotYFromArgs = 0f;
+        float rotZFromArgs = 0f;
+        int countFromArgs = 1;
         switch (args.size()) {
+            case 7:
+                try {
+                    rotZFromArgs = Float.parseFloat(args.get(6));
+                    rotYFromArgs = Float.parseFloat(args.get(5));
+                    rotXFromArgs = Float.parseFloat(args.get(4));
+                } catch (NumberFormatException e) {
+                    CommandHandler.sendMessage(targetPlayer, "these rotation coords are invalid, default to 0");
+                }
+            case 4:
+                try {
+                    countFromArgs = Integer.parseInt(args.get(3));
+                } catch (NumberFormatException e) {
+                    CommandHandler.sendMessage(targetPlayer, "this value of count is invalid, default to 1");
+                }
+            case 3:
+                try {
+                    heightFromArgs = Float.parseFloat(args.get(2));
+                } catch (NumberFormatException e) {
+                    CommandHandler.sendMessage(targetPlayer, "this height from self is an invalid number,default to 0");
+                }
             case 2:
                 try {
-                    radiusFromArgs = Integer.parseInt(args.get(1));
+                    radiusFromArgs = Float.parseFloat(args.get(1));
                 } catch (Exception e) {
-                    CommandHandler.sendMessage(targetPlayer, "the input for radius is invalid");
+                    CommandHandler.sendMessage(targetPlayer, "the input for radius is invalid, default to 0");
                 }
             case 1:
                 // checkNone
@@ -69,6 +96,9 @@ public class AttackModifierCommand implements CommandHandler {
                 } catch (Exception e) {
                     CommandHandler.sendMessage(targetPlayer, "the input for itemId/gadgetId/monsterId is invalid");
                 }
+                break;
+            default:
+                CommandHandler.sendMessage(targetPlayer, "[IdOfObject] [radiusSpawnFromSelf] [height] [count] [rotXupwards] [rotYHorizontalFromSELF] [rotZlastrot]\n\nID of object is the id of the object in the handbook\nradiusSpawnFromSelf is the distance between u and the object spawned when atking\nheight is the distance above u the gadget is spawned\nrotX change the vertical rotation of object\nrotY changed the horizontal rotation from where u are facing [180 will shoot behind u always]\nrotZ is the 3rd cross vector rotation to rot y and x");
         }
 
         //checks if is weapon or itemid coz some people may want spawn wishes while atking or smth;
@@ -90,18 +120,23 @@ public class AttackModifierCommand implements CommandHandler {
         } else if (monsterData != null) {
             monsterId = monsterData.getId();
             gadgetId = 0;
-            monsterId = 0;
+            itemId = 0;
         }
 
         radius = radiusFromArgs;
+        rotX = rotXFromArgs;
+        rotY = rotYFromArgs;
+        rotZ = rotZFromArgs;
+        height = heightFromArgs;
+        count = countFromArgs;
 
         //modifies last used gadget for future attacks
         if (gadgetId != 0) {
-            CommandHandler.sendMessage(targetPlayer, "Future attacks are now infused with:" + "\n\nGadgetID : " + gadgetId);
+            CommandHandler.sendMessage(targetPlayer, "Future attacks are now infused with:" + "\n\nGadgetID : " + gadgetId + "\n\nWITH SETTINGS:\nRadius from self : " + radius + "\nHeight : " + height + "\nCount : " + count + "\nRotX,RotY,RotZ = " + List.of(rotX,rotY,rotZ).toString());
         } else if (itemId != 0) {
-            CommandHandler.sendMessage(targetPlayer, "Future attacks are now infused with:" + "\n\nItemID : " + itemId);
+            CommandHandler.sendMessage(targetPlayer, "Future attacks are now infused with:" + "\n\nItemID : " + itemId + "\n\nWITH SETTINGS:\nRadius from self : " + radius + "\nHeight : " + height + "\nCount : " + count);
         } else if (monsterId != 0) {
-            CommandHandler.sendMessage(targetPlayer, "Future attacks are now infused with:" + "\n\nMonsterID : " + monsterId);
+            CommandHandler.sendMessage(targetPlayer, "Future attacks are now infused with:" + "\n\nMonsterID : " + monsterId  + "\n\nWITH SETTINGS:\nRadius from self : " + radius + "\nHeight : " + height + "\nCount : " + count);
         }
     }
 
@@ -114,93 +149,80 @@ public class AttackModifierCommand implements CommandHandler {
         int addedAttackGadgetId = 0; // Default of no gadget
         int addedAttackItemId = 0; // Default of no gadget
         int addedAttackMonsterId = 0; // Default of no gadget
+        float addedAttackRotX = 0;
+        float addedAttackRotY = 0;
+        float addedAttackRotZ = 0;
+        float addedAttackHeight = 0;
+        float addedAttackRadius = 0;
+        int addedAttackCount = 1;
         
         // Currently will only damage the player
         switch (skillId) { // For Raiden
             case 10521: // Basic attack
                 addedAttackGadgetId = 42906105;
-                addedAttackItemId = 0;
-                addedAttackMonsterId = 0;
+                addedAttackRadius = 0.3f;
                 break;
             case 10522: // Elemental skill
                 addedAttackGadgetId = 42906108;
-                addedAttackItemId = 0;
-                addedAttackMonsterId = 0;
                 break;
             case 10525: // Burst
                 addedAttackGadgetId = 42906119;
-                addedAttackItemId = 0;
-                addedAttackMonsterId = 0;
                 break;
             default:
                 addedAttackGadgetId = gadgetId;  //omg tracks last inputted gadget
                 addedAttackItemId = itemId;
                 addedAttackMonsterId = monsterId;
+                addedAttackRotX = rotX;
+                addedAttackRotY = rotY;
+                addedAttackRotZ = rotZ;
+                addedAttackHeight = height;
+                addedAttackRadius = radius;
+                addedAttackCount = count;
                 break;
         }
 
         // Try to set position in front of player to not get hit
-        // Rotation only cares about y coord, and west is 0, going clockise to 360.
-        //var radius = Math.sqrt(1 * 0.2 / Math.PI); //distance from self
         Position target = new Position(pos);                     //create temporary position from player position
-        double angle = rot.getY();                 //attempts to get y coord to add a few units of radius to pos in that rot direction
-        /*double basicAngle = 0.0;
-        //String ASTC = "";
-        int multSin = 0;
-        int multCos = 0;
+        double angle = rot.getY() + addedAttackRotY;                 //attempts to get y coord to add a few units of radius to pos in that rot direction
 
-        if (angle <= 90.0) {
-            basicAngle = angle;
-            //ASTC = "A";
-            multSin = 1;
-            multCos = 1;
-        } else if (angle > 90.0 && angle <= 180.0) {
-            basicAngle = angle - 90.0;
-            //ASTC = "S";
-            multSin = 1;
-            multCos = -1;
-        } else if (angle > 180.0 && angle <= 270.0) {
-            basicAngle = angle - 180.0;
-            //ASTC = "T";
-            multSin = -1;
-            multCos = -1;
-        } else if (angle > 270.0 && angle <= 360.0) {
-            basicAngle = angle - 270.0;
-            //ASTC = "C";
-            multSin = -1;
-            multCos = 1;
-        }*/
-
-        double r = radius;                         //removed idk sqrt, set to radius from self.
+        Position addedAttackRotation = new Position(addedAttackRotX,rot.getY(),addedAttackRotZ);
+        double r = addedAttackRadius;                         //removed idk sqrt, set to radius from self.
         target.addX((float) (r * Math.sin(Math.PI/180 * angle)));
+        target.addY(addedAttackHeight);
         target.addZ((float) (r * Math.cos(Math.PI/180 * angle)));
-        //target.addX((float) (r * Math.sin(basicAngle) * (multSin)));
-        //target.addZ((float) (r * Math.cos(basicAngle) * (multCos)));
-        
+
+
+
         // Only spawn on match
         if (addedAttackGadgetId != 0) {
-            //EntityGadget attGadget = new EntityGadget(scene, addedAttackGadgetId, target, rot);
-            EntityVehicle attGadget = new EntityVehicle(scene, session.getPlayer(), addedAttackGadgetId, 1, target, rot);
-            // Silly way to track gadget alive time -_-
-            int currTime = (int)(System.currentTimeMillis() - 1665393100);
-            attGadget.setGroupId(currTime);
-            //activeGadgets.add(attGadget);
-            activeVehicles.add(attGadget);
-            // Try to make it not hurt self
-            scene.addEntity(attGadget);
-            attGadget.setFightProperty(2001, 0);
-            attGadget.setFightProperty(1, 0);
-            scene.addEntity(attGadget);
+            for (int i = 1 ; i <= addedAttackCount  ; i++) {
+                //EntityGadget attGadget = new EntityGadget(scene, addedAttackGadgetId, target, rot);
+                EntityVehicle attGadget = new EntityVehicle(scene, session.getPlayer(), addedAttackGadgetId, 1, target, addedAttackRotation);
+                // Silly way to track gadget alive time -_-
+                int currTime = (int)(System.currentTimeMillis() - 1665393100);
+                attGadget.setGroupId(currTime);
+                //activeGadgets.add(attGadget);
+                activeVehicles.add(attGadget);
+                // Try to make it not hurt self
+                scene.addEntity(attGadget);
+                attGadget.setFightProperty(2001, 0);
+                attGadget.setFightProperty(1, 0);
+                scene.addEntity(attGadget);
+            }
             //CommandHandler.sendMessage(session.getPlayer(), "DEBUG:\n\nAVATAR POS: " + pos.getX() + " , " + pos.getY() + " , " + pos.getZ() + "\nAVATAR ROT: " + rot.getX() + " , " + rot.getY() + " , " + rot.getZ() + "\nGADGET POS: " + target.getX() + " , " + target.getY() + " , " + target.getZ() + "\nGADGET ROT: " + rot.toString());
         }
         if (addedAttackItemId != 0) {
             target.addY(3f);
-            EntityItem attItem = new EntityItem(scene, session.getPlayer(), GameData.getItemDataMap().get(addedAttackItemId), target, 1);
-            scene.addEntity(attItem);
+            for (int i = 1 ; i <= addedAttackCount  ; i++) {
+                EntityItem attItem = new EntityItem(scene, session.getPlayer(), GameData.getItemDataMap().get(addedAttackItemId), target, addedAttackCount);
+                scene.addEntity(attItem);
+            }
         }
         if (addedAttackMonsterId != 0) {
-            EntityMonster attMonster = new EntityMonster(scene,GameData.getMonsterDataMap().get(addedAttackMonsterId), target, 90);
-            scene.addEntity(attMonster);
+            for (int i = 1 ; i <= addedAttackCount  ; i++) {
+                EntityMonster attMonster = new EntityMonster(scene,GameData.getMonsterDataMap().get(addedAttackMonsterId), target, 90); 
+                scene.addEntity(attMonster);
+            }
         }
 
         // Remove all gadgets when list not empty
